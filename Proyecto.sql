98397e4a668183dbc3d98fbf908c5190a37e9fc7
Franderpro <rfrander11@gmail.com>
2024-08-28 08:33:54 -0600

    avance de consultas

--- Proyecto.sql
+++ Proyecto.sql
@@ -70,7 +70,6 @@ VALUES
     (1, '104', 'Doble', 85.00, TRUE),
     (1, '105', 'Sencilla', 55.00, TRUE),
     (1, '106', 'Suite', 125.00, TRUE),
-
     -- Hotel 2 (Hotel Las Brisas)
     (2, '201', 'Doble', 70.00, TRUE),
     (2, '202', 'Sencilla', 40.00, TRUE),
@@ -78,7 +77,6 @@ VALUES
     (2, '204', 'Doble', 75.00, TRUE),
     (2, '205', 'Sencilla', 45.00, TRUE),
     (2, '206', 'Suite', 115.00, TRUE),
-
     -- Hotel 3 (Hotel Mari)
     (3, '301', 'Sencilla', 70.00, TRUE),
     (3, '302', 'Suite', 180.00, TRUE),
@@ -86,7 +84,6 @@ VALUES
     (3, '304', 'Sencilla', 75.00, TRUE),
     (3, '305', 'Suite', 185.00, TRUE),
     (3, '306', 'Doble', 95.00, TRUE),
-
     -- Hotel 4 (Hotel La Paz)
     (4, '401', 'Doble', 80.00, TRUE),
     (4, '402', 'Sencilla', 50.00, TRUE),
@@ -94,7 +91,6 @@ VALUES
     (4, '404', 'Doble', 85.00, TRUE),
     (4, '405', 'Sencilla', 55.00, TRUE),
     (4, '406', 'Suite', 125.00, TRUE),
-
     -- Hotel 5 (Hotel El Paraíso)
     (5, '501', 'Doble', 70.00, TRUE),
     (5, '502', 'Sencilla', 40.00, TRUE),
@@ -102,7 +98,6 @@ VALUES
     (5, '504', 'Doble', 75.00, TRUE),
     (5, '505', 'Sencilla', 45.00, TRUE),
     (5, '506', 'Suite', 115.00, TRUE),
-
     -- Hotel 6 (Hotel Vista Hermosa)
     (6, '601', 'Doble', 90.00, TRUE),
     (6, '602', 'Sencilla', 60.00, TRUE),
@@ -110,7 +105,6 @@ VALUES
     (6, '604', 'Doble', 95.00, TRUE),
     (6, '605', 'Sencilla', 65.00, TRUE),
     (6, '606', 'Suite', 145.00, TRUE),
-
     -- Hotel 7 (Hotel Monteverde)
     (7, '701', 'Doble', 85.00, TRUE),
     (7, '702', 'Sencilla', 55.00, TRUE),
@@ -172,10 +166,6 @@ DELIMITER ;
 -- Llamada al procedimiento para eliminar una reserva
 CALL eliminacion_de_reserva(3);
 -- Consultas para verificar los datos
-
-SELECT * FROM Reservas WHERE FechaInicio = CURDATE();
-SELECT * FROM Reservas;
-SELECT CURDATE();
 
 
 
