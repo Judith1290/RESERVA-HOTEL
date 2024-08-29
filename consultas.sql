d25fa560eee014d8759c180a3e01d537f31c4222
Franderpro <rfrander11@gmail.com>
2024-08-29 12:51:39 -0600

    modificacion de consultas

--- consultas.sql
+++ consultas.sql
@@ -1,50 +1,46 @@
-USE reserva_hotel
-
--- habitaciones disponibles
+USE reserva_hotel;
+-- 1. Consulta para contar las habitaciones disponibles en un hotel específico
+-- Esta consulta cuenta el número de habitaciones disponibles en el hotel con HotelID = 1.
 SELECT COUNT(*) AS HabitacionesDisponibles
-FROM Habitaciones
-WHERE HotelID = 1 AND Disponible = TRUE;
-
--- busqueda de hoteles por ubicacion segun el nombre de la ubicacion
-SELECT * 
+FROM Habitaciones ha
+WHERE HotelID = 1
+AND Disponible = TRUE
+AND ha.HabitacionID NOT IN (
+    SELECT r.HabitacionID
+    FROM Reservas r
+    WHERE r.FechaInicio <= '2024-08-15' AND r.FechaFin >= '2024-08-15'
+)
+-- 2. Consulta para buscar hoteles por ubicación según el nombre de la ubicación
+-- Esta consulta busca hoteles cuya ubicación comienza con 'Punt'.
+SELECT *
 FROM Hoteles
-<<<<<<< HEAD
 WHERE Ubicación LIKE 'Punt%';
-=======
-WHERE Ubicación LIKE 'Punta%';
-
--- Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
-SELECT * 
-FROM reservas 
+-- 3. Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior
+-- Esta consulta obtiene todas las reservas realizadas por el usuario con el email 'mari.garcia@gmail.com' en el mes anterior.
+SELECT *
+FROM Reservas
 WHERE UsuarioID = (
-    SELECT `UsuarioID`
-    FROM Usuarios 
+    SELECT UsuarioID
+    FROM Usuarios
     WHERE Email = 'mari.garcia@gmail.com'
-) 
-AND FechaFin < '2024-08-18';
-
--- Consulta para identificar el hotel con la mayor ocupación en el mes anterior.
-SELECT 
+)
+AND FechaFin < CURDATE() AND FechaInicio >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH);
+-- 4. Consulta para identificar el hotel con la mayor ocupación en el mes anterior
+-- Esta consulta identifica el hotel con la mayor cantidad de reservas en el mes anterior.
+SELECT
     ho.HotelID,
     COUNT(r.ReservaID) AS Cantidad_reserva
-FROM 
+FROM
     Reservas r
-INNER JOIN 
+INNER JOIN
     Habitaciones hab ON r.HabitacionID = hab.HabitacionID
-INNER JOIN 
+INNER JOIN
     Hoteles ho ON hab.HotelID = ho.HotelID
-WHERE 
-    MONTH(r.FechaFin) = MONTH(CURDATE() - INTERVAL 1 MONTH)
-    AND YEAR(r.FechaFin) = YEAR(CURDATE() - INTERVAL 1 MONTH)
-GROUP BY 
+WHERE
+    MONTH(r.FechaInicio) = MONTH(CURDATE() - INTERVAL 1 MONTH)
+    AND YEAR(r.FechaInicio) = YEAR(CURDATE() - INTERVAL 1 MONTH)
+GROUP BY
     ho.HotelID
-ORDER BY 
+ORDER BY
     Cantidad_reserva DESC
 LIMIT 1;
-
-
-
-
-
-
->>>>>>> 1108e7be0f18efc298fc16376a2511dfb8c92e4f
